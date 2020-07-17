a177e4cc382bf65312c010561491cd2b9ae0b3cf
hwiyoung <huddy1204@gmail.com>
2020-07-17 09:42:57 +0900

    refactor: Modify the name of parameters & Change the output format

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import numpy as np
 import os
+import glob
 from ba import BA_api
 
 regions = ["yeouido", "pangyo"]
 for region in regions:
-    f = open(region + "_L.txt", "w")
-    f.write("Label_L" + "\t" + "X(m)" + "\t" + "Y(m)" + "\t" + "Z(m)" + "\t"
-            + "Yaw(deg)" + "\t" + "Pitch(deg)" + "\t" + "Roll(deg)" + "\t"
-            + "Omega(deg)" + "\t" + "Phi(deg)" + "\t" + "Kappa(deg)" + "\t"
-            + "Label_R" + "\t" + "X(m)" + "\t" + "Y(m)" + "\t" + "Z(m)" + "\t"
-            + "Yaw(deg)" + "\t" + "Pitch(deg)" + "\t" + "Roll(deg)" + "\t"
-            + "Omega(deg)" + "\t" + "Phi(deg)" + "\t" + "Kappa(deg)" + "\n")
+    f = open(region + "_L.csv", "w")
+    f.write("Label_L" + "," + "X(m)" + "," + "Y(m)" + "," + "Z(m)" + ","
+            + "Yaw(deg)" + "," + "Pitch(deg)" + "," + "Roll(deg)" + ","
+            + "Omega(deg)" + "," + "Phi(deg)" + "," + "Kappa(deg)" + ","
+            + "Label_R" + "," + "X(m)" + "," + "Y(m)" + "," + "Z(m)" + ","
+            + "Yaw(deg)" + "," + "Pitch(deg)" + "," + "Roll(deg)" + ","
+            + "Omega(deg)" + "," + "Phi(deg)" + "," + "Kappa(deg)" + "\n")
 
     # 1. Load a csv file
     reader = np.loadtxt("netvlad/retrieved naverlabs " + region + " image indices.csv", dtype=np.int, delimiter=",")
@@ -23,10 +24,12 @@ for region in regions:
     query_L_path = ["" for i in range(len(query_imgs))]
     query_R_path = ["" for i in range(len(query_imgs))]
     for idx_q in range(len(query_imgs)):
-        query_L_idx = region + str(idx_q).zfill(2) + "/049_L.png"
-        query_R_idx = region + str(idx_q).zfill(2) + "/049_R.png"
-        query_L_path[idx_q] = os.path.join(query_dir, query_L_idx)
-        query_R_path[idx_q] = os.path.join(query_dir, query_R_idx)
+        query_path = glob.glob(os.path.join(query_dir, region + str(idx_q).zfill(2)) + "/*.png")
+        query_L_path[idx_q] = query_path[-2]
+        query_R_path[idx_q] = query_path[-1]
+
+        query_L_label = os.path.join(query_L_path[idx_q].split("\\")[1], query_L_path[idx_q].split("\\")[2])
+        query_R_label = os.path.join(query_R_path[idx_q].split("\\")[1], query_R_path[idx_q].split("\\")[2])
 
         # 3. Select db images using result of step 2
         # 3-1. Select front & back 50 db images of result of step 2
@@ -54,17 +57,17 @@ for region in regions:
         cam_L, eo_L, cam_R, eo_R = ba_api.alignphotos(images_to_process, region)
 
         if len(eo_L) == 0 or len(eo_R) == 0:
-            f.write(query_L_idx + "\t" + str(0) + "\t" + str(0) + "\t" + str(0) + "\t"
-                    + str(0) + "\t" + str(0) + "\t" + str(0) + "\t" + str(0) + "\t" + str(0) + "\t" + str(0) + "\t"
-                    + query_R_idx + "\t" + str(0) + "\t" + str(0) + "\t" + str(0) + "\t"
-                    + str(0) + "\t" + str(0) + "\t" + str(0) + "\t" + str(0) + "\t" + str(0) + "\t" + str(0) + "\n")
+            f.write(query_L_label + "," + str(0) + "," + str(0) + "," + str(0) + ","
+                    + str(0) + "," + str(0) + "," + str(0) + "," + str(0) + "," + str(0) + "," + str(0) + ","
+                    + query_R_label + "," + str(0) + "," + str(0) + "," + str(0) + ","
+                    + str(0) + "," + str(0) + "," + str(0) + "," + str(0) + "," + str(0) + "," + str(0) + "\n")
         else:
-            f.write(query_L_idx + "\t" + str(eo_L[0]) + "\t" + str(eo_L[1]) + "\t" + str(eo_L[2]) + "\t"
-                    + str(eo_L[3]) + "\t" + str(eo_L[4]) + "\t" + str(eo_L[5]) + "\t"
-                    + str(eo_L[6]) + "\t" + str(eo_L[7]) + "\t" + str(eo_L[8]) + "\t"
-                    + query_R_idx + "\t" + str(eo_R[0]) + "\t" + str(eo_R[1]) + "\t" + str(eo_R[2]) + "\t"
-                    + str(eo_R[3]) + "\t" + str(eo_R[4]) + "\t" + str(eo_R[5]) + "\t"
-                    + str(eo_R[6]) + "\t" + str(eo_R[7]) + "\t" + str(eo_R[8]) + "\n")
+            f.write(query_L_label + "," + str(eo_L[0]) + "," + str(eo_L[1]) + "," + str(eo_L[2]) + ","
+                    + str(eo_L[3]) + "," + str(eo_L[4]) + "," + str(eo_L[5]) + ","
+                    + str(eo_L[6]) + "," + str(eo_L[7]) + "," + str(eo_L[8]) + ","
+                    + query_R_label + "," + str(eo_R[0]) + "," + str(eo_R[1]) + "," + str(eo_R[2]) + ","
+                    + str(eo_R[3]) + "," + str(eo_R[4]) + "," + str(eo_R[5]) + ","
+                    + str(eo_R[6]) + "," + str(eo_R[7]) + "," + str(eo_R[8]) + "\n")
 
         # 5. System calibration
         print("System calibration")
